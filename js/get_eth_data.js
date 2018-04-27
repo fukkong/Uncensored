19a8d3c7a7a895a7c9ae8fc53f277c4c27727f90
Fukkong kyu <rivirtu@gmail.com>
2018-04-28 06:46:32 +0900

    style: merge two js file to one.

--- js/get_eth_data.js
+++ js/get_eth_data.js
@@ -3,8 +3,6 @@
 var ethers = require('ethers')
 var providers = ethers.providers
 
-const utf8module = require('./utf8')
-
 var provider = providers.getDefaultProvider('ropsten')
 
 var address = '0x44938b01da1feb3f6fa1cf38870ee564e25d9bf3'
@@ -33,6 +31,33 @@ var data
 etherScanProvider.getHistory(address, startBlock, endBlock).then(function (history) {
   console.log(history)
   data = history[1]['data']
-  console.log(decodeURIComponent(hexToString(data)))
+  console.log(utf8Decode(hexToString(data)))
 })
+
+function utf8Decode (utf8String) {
+  if (typeof utf8String != 'string') throw new TypeError('parameter ‘utf8String’ is not a string')
+  // note: decode 3-byte chars first as decoded 2-byte strings could appear to be 3-byte char!
+  const unicodeString = utf8String.replace(
+    /[\u00e0-\u00ef][\u0080-\u00bf][\u0080-\u00bf]/g,  // 3-byte chars
+    function (c) {  // (note parentheses for precedence)
+      var cc = ((c.charCodeAt(0) & 0x0f) << 12) | ((c.charCodeAt(1) & 0x3f) << 6) | ( c.charCodeAt(2) & 0x3f)
+      return String.fromCharCode(cc)
+    }
+  ).replace(
+    /[\u00c0-\u00df][\u0080-\u00bf]/g,                 // 2-byte chars
+    function (c) {  // (note parentheses for precedence)
+      var cc = (c.charCodeAt(0) & 0x1f) << 6 | c.charCodeAt(1) & 0x3f
+      return String.fromCharCode(cc)
+    }
+  )
+  return unicodeString
+}
+
+function hexToString (hex) {
+  var string = ''
+  for (var i = 0; i < hex.length; i += 2) {
+    string += String.fromCharCode(parseInt(hex.substr(i, 2), 16))
+  }
+  return string
+}
 
